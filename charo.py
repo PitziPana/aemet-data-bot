df78c0595ed67c0b6290e106c3450df2d31c8c71
PitziPana <160005969+PitziPana@users.noreply.github.com>
2024-06-26 23:19:58 +0200

    Update charo.py

--- charo.py
+++ charo.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import telebot
 import pytz
@@ -28,7 +29,7 @@ def obtener_datos_meteorologicos(api_key, estacion_id):
 
 def enviar_datos_telegram(token, chat_id, datos, estacion_nombre):
     bot = telebot.TeleBot(token)
-    if isinstance(datos, dict):  # Asegúrate que los datos son un diccionario
+    if isinstance(datos, dict):  # Asegúrate de que los datos son un diccionario
         utc_time = datetime.strptime(datos['fint'], '%Y-%m-%dT%H:%M:%S%z')
         local_time = utc_time.astimezone(pytz.timezone('Europe/Madrid'))  # Convertir a hora local de Madrid
 
@@ -52,14 +53,20 @@ def enviar_datos_telegram(token, chat_id, datos, estacion_nombre):
         bot.send_message(chat_id, "Error: " + str(datos))
 
 def main():
-    api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKNDk0OEBpY2xvdWQuY29tIiwianRpIjoiMDU4ZDJiNzAtNGJiNC00MWE2LTk1MzEtZmJmOWZhY2M5NmRjIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3MTMyNDM4ODUsInVzZXJJZCI6IjA1OGQyYjcwLTRiYjQtNDFhNi05NTMxLWZiZjlmYWNjOTZkYyIsInJvbGUiOiIifQ.2QEECrTNbTmbBBo3hQCrI1sXu8Q8rHxUzT4q_-kfwxE'
+    api_key = os.getenv('API_KEY')
+    token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
+
+    # Imprimir las variables de entorno para depuración (eliminar estas líneas en producción)
+    print(f"API_KEY: {api_key}")
+    print(f"TELEGRAM_TOKEN: {token}")
+    print(f"TELEGRAM_CHAT_ID: {chat_id}")
+
     estaciones = {
         'Bilbao': '1082',
         'Mayorga': '2664B',
-        'Ayamonte': '4642E'  # Asegúrate de que el ID está correcto y es '0259'
+        'Ayamonte': '4642E'
     }
-    token = '6659256025:AAFK3y_PbW3zhGzURyEc9v-7cZ1v9LwvNpc'
-    chat_id = 317007077
 
     for nombre, estacion_id in estaciones.items():
         datos_meteorologicos = obtener_datos_meteorologicos(api_key, estacion_id)
@@ -67,4 +74,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
